ba6bc963bdeeb98e2d914bbb839e9834bc3fc553
宋强 <861085151@qq.com>
2026-01-09 14:33:27 +0800

    fix：修复 2026-01-02 00:00:00 自动转为 2026-01-02的问题

--- src/global/format.js
+++ src/global/format.js
@@ -1955,7 +1955,7 @@ export function genarate(value) {//万 单位格式增加！！！
     else if (isdatetime(value) && (value.toString().indexOf(".") > -1 || value.toString().indexOf(":") > -1 || value.toString().length < 16)){
         v = datenum_local(parseDate(value.toString().replace(/-/g, "/")));
 
-        if(v.toString().indexOf(".") > -1){
+        // if(v.toString().indexOf(".") > -1){
             if(value.toString().length > 18){
                 ct.fa = "yyyy-MM-dd hh:mm:ss";
             }
@@ -1965,10 +1965,10 @@ export function genarate(value) {//万 单位格式增加！！！
             else{
                 ct.fa = "yyyy-MM-dd";
             }
-        }
-        else{
-            ct.fa = "yyyy-MM-dd";
-        }
+        // }
+        // else{
+        //     ct.fa = "yyyy-MM-dd";
+        // }
         
         ct.t = "d";
         m = SSF.format(ct.fa, v);
